57055fc0649ae0259049608ebe1843d065b9cb54
michaels234 <mdsimeone@gmail.com>
2023-03-08 22:08:48 +0900

    gh-pages first commit front folder only

--- aws_lambda/test_get_movies.py
+++
@@ -1,5 +0,0 @@
-from get_movies import lambda_handler
-
-
-results = lambda_handler({}, {})
-print(f"{len(results)=}\n{results=}")
--- aws_lambda/test_predict.py
+++
@@ -1,5 +0,0 @@
-from predict import lambda_handler
-
-
-results = lambda_handler({'ind': 1}, {})
-print(f"{len(results)=}\n{results=}")
